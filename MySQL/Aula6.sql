4a68019952d55adcf6ad8dbb71a0fbd0c8fe0293
Tetefanii <stephanie001806@gmail.com>
2025-10-06 20:39:17 -0300

    Add files via upload

--- MySQL/Aula6.sql
+++ MySQL/Aula6.sql
@@ -24,4 +24,30 @@ select * from emp where ename like '%TH%' or ename like '%LL%';
 
 select ename, deptno, sal from emp ORDER BY sal DESC;
 
---subconsulg--
+use escritorio;
+
+select ename, job from emp where job = (select job from emp where ename = 'Jones');
+
+select ename, job, deptno from emp where deptno = (select deptno from emp where ename = 'Martin');
+
+select ename, job from emp where job = (select job from emp where empno = 7369) and sal > (select sal from emp where empno = 7876);
+
+select ename, sal, deptno from emp where sal IN (select min(sal)from emp group by deptno);
+
+select ename, sal, job, deptno from emp where sal > any(select distinct sal from emp where deptno=30);
+
+select ename,sal,job,deptno from emp where sal > ALL (select distinct sal from emp where deptno=30);
+
+select ename, hiredate, job, deptno from emp where deptno = (select deptno from emp where ename = 'Allen');
+
+use emp;
+
+select deptno, ename, job from emp where deptno = (select deptno from dept where dname = 'Sales');
+
+select ename, job, sal from emp where sal > (select avg(sal) from emp);
+
+select ename, sal, hiredate, deptno from emp where deptno = (select deptno from emp where ename = 'Martin') and sal < (select avg(sal) from emp);
+
+select e.ename,e.job,d.dname from emp e,dept d where e.deptno = d.deptno;
+
+select emp.ename,emp.job,dept.dname from emp,dept where emp.deptno=dept.deptno;
